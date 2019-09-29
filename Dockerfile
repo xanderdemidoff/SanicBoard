60b6cbc1729514013ebf0178a936eade23dd41c1
Alexander Demidov <xanderdemidoff@gmail.com>
2019-09-29 16:38:44 +0300

    docker-compose network error fixes

--- Dockerfile
+++ Dockerfile
@@ -9,4 +9,6 @@ ADD config.py main.py /workdir/
 RUN apt-get install libpq-dev \
     && pip3 install --no-cache-dir -r /workdir/requirements.txt
 
+EXPOSE 8000
+
 CMD python3.7 main.py
--- config.py
+++ config.py
@@ -1,3 +1,2 @@
 # Scheme: "postgres+psycopg2://<USERNAME>:<PASSWORD>@<IP_ADDRESS>:<PORT>/<DATABASE_NAME>"
-DATABASE_URI = 'postgres+psycopg2://postgres:example@localhost:5432/postgres'
-
+DATABASE_URI = "postgres+psycopg2://docker:docker@postgres:5432/docker"
